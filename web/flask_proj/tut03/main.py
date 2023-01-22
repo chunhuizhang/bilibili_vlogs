20ae1c298241fde1468f0342cc22be61f30a8a7d
chzhang <zch921005@126.com>
2023-01-22 17:10:16 +0800

    前后端分离

--- web/flask_proj/tut03/main.py
+++ web/flask_proj/tut03/main.py
@@ -1,23 +1,30 @@
 
-from flask import Flask
+from flask import Flask, render_template
+import matplotlib.pyplot as plt
+import io
+import base64
+import random
 
 app = Flask(__name__)
 
 
-@app.route('/about')
-def about():
-    return 'The about page'
+def hello():
+    plt.plot([random.randint(0, 10) for _ in range(10)])
+    plt.show()
 
 
-@app.route('/blog')
-def blog():
-    return 'The blog page'
-
-
-@app.route('/blog/<string: blog_id>')
-def blogpost(blog_id):
-    return f'This is blog post number {blog_id}'
+@app.route('/')
+def hello2():
+    fig, ax = plt.subplots(1, 1)
+    ax.plot([random.randint(0, 10) for _ in range(10)])
+    buf = io.BytesIO()
+    fig.savefig(buf, format='png')
+    data = base64.b64encode(buf.getbuffer()).decode("ascii")
+    # print(data)
+    # return f"<img src='data:image/png;base64,{data}'/>"
+    return render_template('hello.html', image=f'data:image/png;base64,{data}')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # hello()
+    app.run()
